c041dd2829bbb26bc20b40a0ebe13b09759dca1e
Vlad Soltys <99182280+vladsoltys@users.noreply.github.com>
2025-08-10 11:43:05 +0000

    error_msg_added

--- rm_add_element_DOM/logic.js
+++ rm_add_element_DOM/logic.js
@@ -3,27 +3,64 @@ const input = document.querySelector('#inputMsg');
 const addText = document.querySelector('#addBtn');
 const removeText = document.querySelector('#clearBtn');
 const newText = document.querySelector('#messages');
+const errorMsg = document.querySelector('#errorMessages');
 
 // Функція кнопки, що дадає нове повідомлення з іменем користувача та часом
 addText.addEventListener('click', () => {
-  const div = document.createElement('div');
-  const btn = document.createElement('button');
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  
-  div.textContent = `${inputName.value}: ${input.value} - ${hours}:${minutes}`;
-  btn.textContent = "Видалити";
+  const name = inputName.value.trim();
+  const message = input.value.trim();
 
-  // Кнопка видалення для кожного повідомлення окремо 
-  btn.addEventListener('click', () => {
-    div.remove();
-  });
-  
-  newText.appendChild(div);
-  div.appendChild(btn);
-  input.value = '';
-  inputName.value = '';
+  // Перевірка на заповненість полів
+
+  // Якщо обидва поля заповнені створюємо нове повідомлення
+  if (name && message) {
+
+    errorMsg.innerHTML = '';
+
+    const div = document.createElement('div');
+    const btn = document.createElement('button');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+
+    div.textContent = `${name.value}: ${message.value} - ${hours}:${minutes}`;
+    btn.textContent = "Видалити";
+
+    // Кнопка видалення для кожного повідомлення окремо 
+    btn.addEventListener('click', () => {
+      div.remove();
+    });
+
+    newText.appendChild(div);
+    div.appendChild(btn);
+    message.value = '';
+    name.value = '';
+  }
+  // Перевірка на заповненість полів, якщо обидва поля пусті
+  else if (!name && !message) {
+    errorMsg.innerHTML = '';
+
+    const div = document.createElement('div');
+    div.textContent = 'Поля не можуть бути пустими'
+    errorMsg.appendChild(div);
+  }
+  // Якщо пусте лише поле імені
+  else if (!name) {
+    errorMsg.innerHTML = '';
+
+    const div = document.createElement('div');
+    div.textContent = 'Поле імені не може бути пустим';
+    errorMsg.appendChild(div);
+  }
+  // Якщо пусте лише поле повідомлення
+  else if (!message) {
+    errorMsg.innerHTML = '';
+
+    const div = document.createElement('div');
+    div.textContent = 'Поле повідомлення не може бути пустим';
+    errorMsg.appendChild(div);
+  }
+
 });
 
 // Кнопка для очищення всіх повідомлень
